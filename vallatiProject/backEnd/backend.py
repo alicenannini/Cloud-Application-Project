a1d0b1dbdef6b5baa17b4a29955cd6d078423c86
marco <marcoparola96@gmail.com>
2020-09-01 12:59:04 +0200

    update backend

--- vallatiProject/backEnd/backend.py
+++ vallatiProject/backEnd/backend.py
@@ -1,5 +1,12 @@
 
 import mysql.connector
+import pika
+
+
+brokerAddr = ""
+brokerConn = ""
+brokerChan = ""
+
 
 # CONNECT/DISCONNECT METHODS
 def connect():
@@ -82,18 +89,46 @@ def putMail(mydb, id, sender, receiver, text):
 
 
 
-def callback():
-
-
-
-
-def main():
-    db = connect()
-    postMail(db, 'Ste', 'Ma', 'ciaone proprio')
-
-if __name__ == "__main__":
-    main()
+def callback(ch, method, properties, body):
+    params = body.decode().split('//')
+    print('callback')
     
+    ch.queue_declare('results')
+    db = connect()
+    
+    if(params[0] == 'create'):
+        if(len(params) == 4):
+            created = postMail(db, params[1], params[2], params[3])
+            ch.basic_publish(exchange = '', routing_key = 'results', body = '{"created: "' + str(created) + '}')
+    else:
+        ch.basic_publish(exchange = '', routing_key = 'results', body = '{"error, command not found"}')
+    '''    
+    else if(params[0] == 'update'):
+    
+    else if(params[0] == 'delete'):
+        
+    else if(params[0] == 'read'):
+    '''
+    
+
+    
+    disconnect(db)
+    
+    
+def main():
+    brokerChan.queue_declare(queue='tasks')
+    brokerChan.basic_consume(queue='tasks', on_message_callback=callback, auto_ack=True)
+    print('Back-end ready.')
+    brokerChan.start_consuming()
+
+
+if __name__ == '__main__':
+    credentials = pika.PlainCredentials("root", "root")
+    brokerAddr = "172.16.2.34"
+    parameters = pika.ConnectionParameters(brokerAddr, '5672', '/', credentials)
+    brokerConn = pika.BlockingConnection(parameters)
+    brokerChan = brokerConn.channel()
+    main()
     
     
     
